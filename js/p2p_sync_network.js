9e4e8cbe23dcfc569c3b1a9be3a64aafe0452d68
CluadiaCapella <cluadiacapella@users.noreply.github.com>
2026-09-01 23:04:34 -0400

    Fix top bar safe-area top inset padding for mobile screens and add OpenRelay TCP TURN servers for NAT-traversing WebRTC connections

--- js/p2p_sync_network.js
+++ js/p2p_sync_network.js
@@ -548,8 +548,12 @@ async function initLocalPeerServer() {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
-            { urls: 'stun:stun.services.mozilla.com' },
-            { urls: ['turn:eu-0.turn.peerjs.com:3478', 'turn:us-0.turn.peerjs.com:3478'], username: 'peerjs', credential: 'peerjsp' }
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
+            { urls: 'stun:global.stun.twilio.com:3478' },
+            { urls: 'turn:openrelay.metered.ca:80', username: 'openrelay', credential: 'openrelay' },
+            { urls: 'turn:openrelay.metered.ca:443', username: 'openrelay', credential: 'openrelay' },
+            { urls: 'turn:openrelay.metered.ca:443?transport=tcp', username: 'openrelay', credential: 'openrelay' }
           ]
         }
       });
